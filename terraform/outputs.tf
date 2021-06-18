194444e2df8b19deca8ca85d05b734dc2a10a951
Marcos F. Villa <mvicha@gmail.com>
2021-06-18 10:58:57 +0000

    Add ServerAliveInterval every 5 seconds to keep ssh connection open

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -72,3 +72,9 @@ output "ghost_https_service_url" {
 output "WARNING" {
   value = length(var.public_key_path) > 0 && length(var.private_key_path) == 0 ? "Estás pasando un valor para PUBLIC_KEY_PATH pero PRIVATE_KEY_PATH está vacío" : (length(var.private_key_path) > 0 && length(var.public_key_path) == 0 ? "Estás pasando un valor PRIVATE_KEY_PATH pero PUBLIC_KEY_PATH está vacío" : "")
 }
+
+
+# Ansible command line
+output "ansible_exec_command" {
+  value = "ansible-playbook -i ../ansible/hosts -e jump_host='${data.azurerm_public_ip.pi_k8s.ip_address}' -e admin_user='${var.ssh_user}' -e subnet_cidr_private='${azurerm_subnet.sn_k8s_private.address_prefixes[0]}' -e private_lan_master='${azurerm_network_interface.nic_k8s_master.private_ip_address}' -e private_lan_node01='${var.workers["Node01"].ip}' -e private_lan_node02='${var.workers["Node02"].ip}' -e internal_private_key_file='${local.internal_private_key_file}' -e external_private_key_file='${local.external_private_key_file}' -e ghost_url='${data.azurerm_public_ip.pi_k8s.fqdn}' ../ansible/playbook.yml"
+}
