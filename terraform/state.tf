9d421702fcdd34015d582c60c54fc2e5976c1193
Marcos F. Villa <mvicha@gmail.com>
2021-06-30 21:36:14 +0200

    Remove state.tf from repository

--- terraform/state.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "tstate"
-    storage_account_name  = "tstate6619"
-    container_name        = "tstate"
-    key                   = "terraform.tfstate"
-    access_key            = "uQF+DG+zzytI/NPuA5mwruhOo1mwo0HlQMkt6OYoEnJsJbGLkvK9y1i+c/DaHZljMF8tAyQghFc44TqtjnvJfw=="
-  }
-}
